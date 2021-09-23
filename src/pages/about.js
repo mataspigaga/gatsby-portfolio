7aa6219f2dbf04c7f6e968eace9fc0ee06ba64e8
Matas Pigaga <mataspigaga@gmail.com>
2021-09-23 03:43:34 -0400

    added mobile breakpoint / updated About content

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,13 +12,12 @@ const AboutPage = () => {
         </div>
         <p>
           Hi, I'm a web developer exploring the MERNG stack. Right now, I am
-          focusing on building this portfolio site using Gatsby and integrated a
-          headless CMS (content management system) called Sanity.
+          focusing on building this portfolio site using Gatsby and an
+          integrated headless CMS (content management system) called Sanity.
         </p>
         <p>
-          I will also try to integrate my existing blog MDX files into this
-          site, as well as all of my JS challenges I have created onto separate
-          pages or even one page.
+          On this project, my goals are to make it a single page experience with
+          blog article previews, links to various projects, and a contact form.
         </p>
         <section className={classes.socialMediaLinks}>
           <h3>Get In Touch</h3>
