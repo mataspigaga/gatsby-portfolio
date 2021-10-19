c32482cf38479d7645d13e2cb0cbab94faa8d6ad
Matas Pigaga <mataspigaga@gmail.com>
2021-10-19 00:59:48 -0400

    updated welcome message and posted date format

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,11 +2,17 @@ import React from "react";
 import Layout from "../UI/Layout";
 import AboutMe from "./AboutMe";
 import RecentPosts from "./RecentPosts";
+import {Link} from 'gatsby'
 
 const HomePage = () => {
   return (
     <Layout pageTitle="Home">
-      <p>In the process of building this site with Gatsby.js and Sanity</p>
+      <p>
+        In the process of building this site with Gatsby.js, but I'm mostly
+        using it as a place to hold my daily recaps of the{" "}
+        <Link to="/posts/100-days/">#100DaysOfCode</Link> Challenge I'm
+        currently going through.
+      </p>
       <h2>Recent Posts</h2>
       <RecentPosts />
       <h2>About Me</h2>
--- src/pages/posts/{mdx.slug}.js
+++ src/pages/posts/{mdx.slug}.js
@@ -33,7 +33,7 @@ export const query = graphql`
     mdx(id: { eq: $id }) {
       frontmatter {
         title
-        date
+        date(formatString: "MMMM DD, YYYY")
         hero_image_alt
         hero_image_credit_link
         hero_image_credit_text
